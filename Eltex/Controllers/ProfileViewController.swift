e192453177d77dfe6609b2195a8825a25c7b270a
Yersage <yersage@gmail.com>
2022-09-22 16:50:00 +0600

    Initial commit

---
+++ Eltex/Controllers/ProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Eltex
+//
+//  Created by Yersage on 22.09.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
